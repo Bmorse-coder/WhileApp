a14267a6f531806a2b99f107650b8e662c3b20e9
Bmorse-coder <bmorse@appdev.yearup.org>
2023-04-19 14:03:56 -0400

    Add a new file

---
+++ src/main/java/WhileApp.java
@@ -0,0 +1,14 @@
+public class WhileApp {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+
+        while (i<=5){
+            System.out.println("I love Java");
+            i++;
+
+        }
+
+    }
+}
